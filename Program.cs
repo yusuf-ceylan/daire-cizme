e585984a2b0bc31a935660b485d2bd703418a527
Yusuf CEYLAN <yusuf.ceylann@outlook.com.tr>
2023-08-17 09:39:54 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace daire_cizme
+{
+    public class CircleDrawer
+    {
+        public static void DrawCircle(int radius)
+        {
+            double diameter = 2 * radius;
+            double radiusSquared = radius * radius;
+
+            for (int i = -radius; i <= radius; i++)
+            {
+                for (int j = -radius; j <= radius; j++)
+                {
+                    if (i * i + j * j <= radiusSquared)
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Daire yarıçapı: ");
+            int radius = int.Parse(Console.ReadLine());
+
+            CircleDrawer.DrawCircle(radius);
+        }
+    }
+}
